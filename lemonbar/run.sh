de317bdf477b6492ca3916865b972c060d540d61
Adam Tuechler <adamtuechler@gmail.com>
2016-08-29 19:37:43 +1000

    Change height of bars to fix misaligned text, and change bottom text to size=9

--- lemonbar/run.sh
+++ lemonbar/run.sh
@@ -7,11 +7,11 @@ killall lemonbar
 col_top="#101010"
 col_bot="#101010"
 
-dim_top="-g x20"
-dim_bot="-g x18"
+dim_top="-g x21"
+dim_bot="-g x16"
 
 font_top="-f Roboto Mono:style=Light:size=10"
-font_bot=$font_top
+font_bot="-f Roboto Mono:style=Regular:size=9"
 
 ($HOME/.config/lemonbar/top.sh | lemonbar -B $col_top $dim_top "$font_top")&
 ($HOME/.config/lemonbar/bottom.sh | lemonbar -b -B $col_bot $dim_bot "$font_bot")&
