f61f3ff6c7e894299f554b60ef33da9834767376
Adam Tuechler <adamtuechler@gmail.com>
2016-10-21 15:04:56 +1100

    Update font to Ubuntu mono. Reduced height.

--- config/lemonbar/run.sh
+++ config/lemonbar/run.sh
@@ -7,10 +7,10 @@ killall lemonbar
 col_top="#101010"
 col_bot="#101010"
 
-dim_top="-g x21"
+dim_top="-g x18"
 dim_bot="-g x16"
 
-font_top="-f Roboto Mono:style=Light:size=10"
+font_top="-f Ubuntu Mono:style=Regular:size=10"
 font_bot="-f Roboto Mono:style=Regular:size=9"
 
 ($HOME/.config/lemonbar/top.sh | lemonbar -B $col_top $dim_top "$font_top")&
