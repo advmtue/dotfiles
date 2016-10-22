59b58a25c29de1dfdba30a0072249637346caeae
Adam Tuechler <adamtuechler@gmail.com>
2016-10-22 21:42:24 +1100

    Initial commit.
    albumgain.sh
    Script to apply mp3gain to *.mp3 within CWD
    Assume CWD contains only one album

---
+++ scripts/albumgain.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# gainalbum.sh
+#   Script to write replaygain tags of all mp3 files in a dir
+#   Assumes that directory is one album
+
+# Text for identification of modified files
+echo "gainalbum.sh..."
+echo "Files to be modified:"
+for f in *.mp3; do
+  printf "\t$f\n"
+done
+
+# Do some shit
+printf "\nAnalyze and apply? [y/N]: "
+read -n 1 selection
+if [[ "$selection" == "y" ]]; then
+  printf "\nAlright boss, doin' shit...\n"
+  find . -name "*.mp3" -exec mp3gain -a -k -s r {} \;
+else
+  printf "\nAborted.\n"
+fi
