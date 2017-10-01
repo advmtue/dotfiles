eed3af8eea55b08961e86a76d1de2cd3c25214fe
Adam Tuechler <adamtuechler@gmail.com>
2017-10-01 21:18:26 +1100

    Moved to icon format

--- scripts/audiolist.sh
+++ scripts/audiolist.sh
@@ -4,11 +4,11 @@
 # Translates an audio device from its name to a nicer one
 
 declare -A output_list
-output_list["hdmi-output-1"]="SCREEN"
-output_list["analog-output-lineout"]="SPEAKER"
-output_list["analog-output-headphones"]="HEADPHONE"
-output_list["none"]="NONE"
-output_list["unknown"]="UNKNOWN"
+output_list["hdmi-output-1"]=""
+output_list["analog-output-lineout"]=""
+output_list["analog-output-headphones"]=""
+output_list["none"]=""
+output_list["unknown"]=""
 
 device_name="unknown"
 
