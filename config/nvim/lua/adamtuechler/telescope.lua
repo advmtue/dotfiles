a199a27e5b46e303ca3ffe139d2134c13e8b3331
Adam Tuechler <ad@mtue.ch>
2024-10-29 08:26:11 +1100

    Use git_files with failover to find_files

--- config/nvim/lua/adamtuechler/telescope.lua
+++ config/nvim/lua/adamtuechler/telescope.lua
@@ -1,5 +1,4 @@
--- Fuzzy finder: files and buffers
---
+-- File and buffer switching and searching
 require('telescope').setup({
 	defaults = {
 		mappings = {
@@ -21,7 +20,21 @@ require('telescope').setup({
 	}
 })
 
+local function select_project_files(limit_to_cwd)
+	limit_to_cwd = limit_to_cwd or false
+
+	vim.fn.system("git rev-parse --is-inside-work-tree")
+	local is_inside_work_tree = vim.v.shell_error == 0
+
+	if is_inside_work_tree then
+		require("telescope.builtin").git_files({ use_git_root = not limit_to_cwd })
+	else
+		require("telescope.builtin").find_files({ hidden = true })
+	end
+end
+
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {}, { hidden = true })
+vim.keymap.set('n', '<leader>ff', select_project_files)
+vim.keymap.set('n', '<leader>fd', function() select_project_files(true) end)
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
