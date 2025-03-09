0ba957a3215ef0b8531541ba93ab97fc9fd45ba8
Adam Tuechler <ad@mtue.ch>
2025-03-09 20:57:17 +1100

    Make <leader>-ff generic (but ignore .git)

--- config/nvim/lua/adamtuechler/telescope.lua
+++ config/nvim/lua/adamtuechler/telescope.lua
@@ -20,25 +20,12 @@ require('telescope').setup({
 	}
 })
 
-local function is_git_repo()
-	vim.fn.system("git rev-parse --is-inside-work-tree")
-
-	return vim.v.shell_error == 0
-end
-
-
-local function select_project_files(limit_to_cwd)
-	limit_to_cwd = limit_to_cwd or false
-
-	if is_git_repo() then
-		require("telescope.builtin").git_files({ use_git_root = not limit_to_cwd })
-	else
-		require("telescope.builtin").find_files({ hidden = true })
-	end
-end
-
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', select_project_files)
-vim.keymap.set('n', '<leader>fd', function() select_project_files(true) end)
+local utils = require('telescope.utils')
+vim.keymap.set('n', '<leader>ff', function()
+	builtin.find_files({ find_command = { "rg", "--ignore", "--hidden", "--glob=!.git/", "--files" } })
+end)
+vim.keymap.set('n', '<leader>fd', function() builtin.find_files({ cwd = utils.buffer_dir() }) end)
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
