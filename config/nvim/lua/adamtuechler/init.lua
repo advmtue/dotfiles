8f57458eb1e983eb5493b75fc72dc322bc305922
Adam Tuechler <ad@mtue.ch>
2025-03-10 08:53:42 +1100

    Add up/down text movements

--- config/nvim/lua/adamtuechler/init.lua
+++ config/nvim/lua/adamtuechler/init.lua
@@ -9,3 +9,9 @@ vim.keymap.set('n', '<C-h>', [[:wincmd h<CR>]], { silent = true })
 vim.keymap.set('n', '<C-j>', [[:wincmd j<CR>]], { silent = true })
 vim.keymap.set('n', '<C-k>', [[:wincmd k<CR>]], { silent = true })
 vim.keymap.set('n', '<C-l>', [[:wincmd l<CR>]], { silent = true })
+
+-- Moving text up/down
+vim.keymap.set('n', '<A-j>', [[:m .+1<CR>==]], { silent = true })
+vim.keymap.set('n', '<A-k>', [[:m .-2<CR>==]], { silent = true })
+vim.keymap.set('v', '<A-j>', [[:m '>+1<CR>gv=gv]], { silent = true })
+vim.keymap.set('v', '<A-k>', [[:m '<-2<CR>gv=gv]], { silent = true })
